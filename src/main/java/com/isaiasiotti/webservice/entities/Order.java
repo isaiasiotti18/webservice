f206b9de0a7a400ddce4296acffcaff9c1368249
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2026-02-17 20:00:22 -0300

    implement Order entity, repository, service, and resource with sample data

--- src/main/java/com/isaiasiotti/webservice/entities/Order.java
+++ src/main/java/com/isaiasiotti/webservice/entities/Order.java
@@ -3,7 +3,8 @@ package com.isaiasiotti.webservice.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
-import jakarta.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +22,8 @@ public class Order implements Serializable {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
   private Instant moment;
 
   @ManyToOne
