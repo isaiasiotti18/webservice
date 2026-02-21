645a0e7478328949b0f5c4496dfcdffc56fbc1dc
isaiasiotti18 <isaiasiottiprofissional@gmail.com>
2026-02-21 18:05:24 -0300

    feat: implement exception handling for UserService; add ResourceExceptionHandler, DatabaseException, and ResourceNotFoundException

--- src/main/java/com/isaiasiotti/webservice/services/UserService.java
+++ src/main/java/com/isaiasiotti/webservice/services/UserService.java
@@ -5,10 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties.Apiversion.Use;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.isaiasiotti.webservice.entities.User;
 import com.isaiasiotti.webservice.repositories.UserRepository;
+import com.isaiasiotti.webservice.services.exceptions.DatabaseException;
+import com.isaiasiotti.webservice.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,15 +26,11 @@ public class UserService {
 
   public User findById(Long id) {
     Optional<User> user = userRepository.findById(id);
-    return user.get();
+    return user.orElseThrow(() -> new ResourceNotFoundException(id));
   }
 
   public User insert(User user) {
     return userRepository.save(user);
-  }
-
-  public void delete(Long id) {
-    userRepository.deleteById(id);
   }
 
   public User update(Long id, User user) {
@@ -44,4 +44,15 @@ public class UserService {
     entity.setEmail(user.getEmail());
     entity.setPhone(user.getPhone());
   }
+
+  public void delete(Long id) {
+    try {
+      userRepository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new ResourceNotFoundException(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DatabaseException(e.getMessage());
+    }
+  }
+
 }
